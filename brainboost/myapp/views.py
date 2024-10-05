8a87a4199e73ba03189cf97b06271982385ba9a1
Bushra Jannat <bjannat202217@bscse.uiu.ac.bd>
2024-10-05 23:51:38 +0600

    react integrated

--- brainboost/myapp/views.py
+++ brainboost/myapp/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate, login as auth_login, logout
 from .models import Courses,Contact,Parent, Quiz, Question, Answer
 # Create your views here.
 
+def index(request):
+    return render(request,'index.html')
+
 def homeview(request):
     username = request.session.get('username')
     user_id = request.session.get('user_id')
